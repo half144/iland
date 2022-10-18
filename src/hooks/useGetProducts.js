2f63985f4586940d072d735ebc70890ab6f43fe6
half144 <99549268+half144@users.noreply.github.com>
2022-10-18 18:28:50 -0300

    Update useGetProducts.js

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const url = "http://localhost:3000";
+const url = "https://ilandback.herokuapp.com/";
 
 const useGetProducts = (endpoint) => {
   const [products, setProducts] = useState();
